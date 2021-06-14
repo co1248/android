fbf3ad18eba8af8893751b97402d7f379e85c2ec
co1248 <83028206+co1248@users.noreply.github.com>
2021-06-14 17:28:57 +0900

    Add files via upload
    0614

---
+++ SampleOrientation/app/src/main/java/org/techtownw/sampleorientation/MainActivity.java
@@ -0,0 +1,42 @@
+package org.techtownw.sampleorientation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showToast("onCreat 호출됨");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        showToast("onStart 호출됨.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        showToast("onstop 호출됨.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        showToast("onDestroy 호출됨.");
+    }
+
+    public void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+    }
+}

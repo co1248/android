fbddf4ad78090688ea3712e429a659e564033336
co1248 <83028206+co1248@users.noreply.github.com>
2021-06-23 17:10:08 +0900

    Add files via upload
    0623

---
+++ SampleDrawer/app/src/main/java/org/techtown/drawer/FragmentCallback.java
@@ -0,0 +1,7 @@
+package org.techtown.drawer;
+
+import android.os.Bundle;
+
+public interface FragmentCallback {
+    void onFragment(int position, Bundle bundle);
+}
---
+++ SampleDrawer/app/src/main/java/org/techtown/drawer/MainActivity.java
@@ -0,0 +1,94 @@
+package org.techtown.drawer;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
+import org.techtown.drawer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, FragmentCallback{
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    DrawerLayout drawer;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.f1) {
+            onFragment(0,null);
+        } else if(id == R.id.f2) {
+            onFragment(1,null);
+        } else if(id == R.id.f3) {
+            onFragment(2,null);
+        }
+        
+        drawer.closeDrawer(GravityCompat.END);
+//        drawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+    @Override
+    public void onFragment(int position, Bundle bundle) {
+        Fragment curFragment = null;
+        
+        if(position == 0) {
+            curFragment = fragment1;
+            toolbar.setTitle("첫번째 화면");
+        } else if(position ==1) {
+            curFragment = fragment2;
+            toolbar.setTitle("두번째 화면");
+        } else if(position ==2) {
+            curFragment = fragment3;
+            toolbar.setTitle("세번째 화면");
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, curFragment).commit();
+
+    }
+}
